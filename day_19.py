cafc8e14d86680c8fdf992be3247a0ceca06240b
Navollas patil <ngpatil04123@gmail.com>
2026-03-06 15:15:34 +0530

    Add files via upload

---
+++ day_19.py
@@ -0,0 +1,29 @@
+# print("Never Give up!!")
+# def greeting():
+#     print("Hello ullas , welcome to python !!")
+# greeting()
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# def sum(a,b):
+#     print(f"the sum of {a} and {b} is : {a+b}")
+# sum(10,20)
+#--------------------------------------------------------------------------------------------------------------------------------------------------------
+# def even_odd():
+#     a=int(input("Enter the number : "))
+#     if a%2==0:
+#         print(f"the {a} is even number")
+#     else:
+#         print(f"the {a} is odd ")
+# even_odd()
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
+def count_vowel(s):
+    count=0
+    for i in s:
+      if i in "aeiouAEIOU":
+         count+=1
+    return count
+
+s=str(input("Enter the string : "))
+result=count_vowel(s)
+print("number of vowels is  :",result)
+
+
